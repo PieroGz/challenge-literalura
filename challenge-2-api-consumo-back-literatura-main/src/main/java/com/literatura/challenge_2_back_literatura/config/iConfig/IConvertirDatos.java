3edb0b5308cb7709cc7eedbb6bf7f0bfb2894af8
PieroGz <129896854+PieroGz@users.noreply.github.com>
2024-07-15 02:41:16 -0500

    Delete challenge-2-api-consumo-back-literatura-main directory

--- challenge-2-api-consumo-back-literatura-main/src/main/java/com/literatura/challenge_2_back_literatura/config/iConfig/IConvertirDatos.java
+++
@@ -1,7 +0,0 @@
-package com.literatura.challenge_2_back_literatura.config.iConfig;
-
-public interface IConvertirDatos {
-
-    <T> T convertirDatosJsonAJava(String json , Class<T> clase); //metodo generico que coje el json y convierte en datos Java ya que no se sabe que retornara
-
-}
